bace2091a33dc5d0ed5caa736beca4eca2e8f4ae
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-16 16:14:11 -0500

    current weather functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -119,3 +119,65 @@ function displayDate() {
     forecastDate5.test(fiveDay5);
 };
 // Create dates and times across page End
+
+// Current weather Start
+function displayCurrentWeather(currentCity) {
+    // display city as a header on current weather area
+    cityDisplayed.innerHTML = currentCity;
+    // fetch first data from weather api
+    featch('https://api.openweathermap.org/data/2.5/weather?q='
+    + currentCity
+    + '&appid='
+    + apiKey
+    + '&units=imperial'
+    )
+    // check if a known city was entered
+    .then(function (weatherResponse) {
+        if (weatherResponse.ok) {
+            weatherResponse.json().then(function (weatherResponse) {
+                // fetch the uv data
+                fetch (
+                    'https://api.openweathermap.org/data/2.5/uvi?appid='
+                    + apiKey
+                    + '&lat='
+                    + weatherResponse.coord.lat
+                    + '&lon='
+                    + weatherResponse.coord.lon 
+                )
+                    .then(function (response ) {
+                        return response.json();
+                    })
+                    .then(function (response) {
+                        // create variables for each data value needed
+                        var uvIndex = response.value
+                        var temperatureValue = Math.round(weatherResponse.main.feels_like);
+                        var humidityValue = weatherResponse.main.humidity;
+                        var windSpeedValue = weatherResponse.wind.speed;
+                        var uvIndexValue = JSON.stringify(uvIndex);
+                        var weatherIcon = weatherResponse.weather[0].icon;
+                        var currentWeatherCondition = weatherResponse.weather[0].icon;
+                        var iconUrl = "http://openweathermap.org/img/wn" + weatherIcon + "@2x.png";
+                        // Append current weather values to span elements w/in thecureent weather area 
+                        currentWeatherCond.textContent = currentWeatherCondition.toUpperCase();
+                        currentCityTemp.textContent = temperatureValue;
+                        currentyCityHumidity.textContent = humidityValue;
+                        currentCityWind.textContent = windSpeedValue;
+                        currentCityUV.textContent = uvIndexValue; // remember that data is provided only for 12pm
+                        // change the color of UV index
+                        if (uvIndexValue < 3) {
+                            currentCityUV.setAttribute("class", "badge badge-success")
+                        } else if (uvIndexValue > 7) {
+                            currentCityUV.setAttribute("class", "badge badge-danger")
+                        } else if (uvIndexValue >= 3 && uvIndexValue <= 7) {
+                            currentCityUV.setAttribute("class", "badge badge-warning")
+                        };
+                        currentCityIcon.setAttribute("src", iconUrl);
+                        cityDisplayed.innerHTML = currentCity;
+                    })
+            })
+        } else {
+            alert("Error: City not Found!")
+        }
+    })
+}
+// Current weather End
